7d6bfc29297900c6d4f3f64dfff1ddb86a0775d1
Iván <114758527+ICasGar@users.noreply.github.com>
2022-11-07 20:19:53 +0100

    Add files via upload

---
+++ 1DAM-Programación/02_EjerciciosT2/O1_EjercicioT2.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pkg01_ejerciciost2;
+
+/**
+ *
+ * @author Iván Castro
+ */
+public class O1_EjercicioT2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String movil;
+        System.out.println("Ordenes:");
+        System.out.println("Enciendete");
+        System.out.println("Apagate");
+        System.out.println("Saca fotos");
+        System.out.println("Preguntas: ");
+        System.out.println("Cual es la resolucion de la camara?");
+        System.out.println("5000px");
+        System.out.println("Cuanta capacidad tienes?");
+        System.out.println("108GB");
+        System.out.println("Tiene Wifi?");
+        System.out.println("Si");
+    }
+    
+}
